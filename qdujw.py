d5440a95488e0bd3ec11695d3757d5005f76b3d3
Kyle <pvc.hao@gmail.com>
2016-02-22 20:07:54 +0800

    个人信息查询，课表以表格展示
    使用 lxml 解析页面

--- qdujw.py
+++ qdujw.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-'''
-@title: 青岛大学教务爬虫
-@author: Kyle
-@time: 2016-1-30
-@update: 2016-2-19
-'''
 
 from PIL import Image
 import io
@@ -12,7 +6,8 @@ import re
 import getpass
 import requests
 import pytesseract
-from bs4 import BeautifulSoup
+from lxml import etree
+from prettytable import PrettyTable
 
 
 class qdujw:
@@ -65,7 +60,7 @@ class qdujw:
 
         # 密码错误
         if re.search(u'\u5bc6\u7801\u4e0d\u5339\u914d', r.text):
-            print '密码不匹配!'
+            print sid + ' 密码不匹配!'
             print
             choice = raw_input('请选择(1.重新输入 2.退出)：')
             if choice == '1':
@@ -77,7 +72,7 @@ class qdujw:
 
         # 用户名不存在
         elif re.search(u'\u4e0d\u5b58\u5728', r.text):
-            print '用户名"' + sid + '"不存在!'
+            print sid + ' 用户名不存在!'
             print
             choice = raw_input('请选择(1.重新输入 2.退出)：')
             if choice == '1':
@@ -95,10 +90,14 @@ class qdujw:
         # 成功
         else:
             userpage = self.s.get(userurl).content
+            name = etree.HTML(userpage.decode(
+                'utf-8', 'ignore')).xpath('/html/body/center/table[1]/tr[1]/td[2]/text()')
+            for n in name:
+                name = n.encode('utf-8')
             userid = re.findall(b'.*?userid=(.*?)"', userpage, re.S)
             for i in userid:
                 self.userid = self.userid * 10 + int(i)
-            print '登录成功！'
+            print name + '登录成功！'
 
     # 查询成绩
     def scores(self):
@@ -132,7 +131,11 @@ class qdujw:
         print
         year = raw_input('请输入查询学年: ')
         term = raw_input('春季学期输入[1],秋季学期输入[2]: ')
-        kburl = 'http://jw.qdu.edu.cn/academic/manager/coursearrange/showTimetable.do'
+        print
+
+        # 使用 PrettyTable 打印表格
+        x = PrettyTable(["周一", "周二", "周三", "周四", "周五", "周六", "周日"])
+        x.padding_width = 1
         get = {
             'id': self.userid,
             'yearid': eval(year + '-' + '1980'),
@@ -141,19 +144,58 @@ class qdujw:
             'sectionType': 'COMBINE'
         }
 
-        kebiao = BeautifulSoup(self.s.get(kburl, params=get).content, "lxml")
-        for kk in kebiao.find_all("td", class_="center"):
-            print kk.get_text()
+        kb = self.s.get(
+            'http://jw.qdu.edu.cn/academic/manager/coursearrange/showTimetable.do', params=get)
+        html = etree.HTML(kb.content)
+
+        kb = html.xpath('//*[@id="timetable"]//*[@class="center"]')
+        i = 0
+        list = []
+        list1 = ['', '', '', '', '', '', '']
+        for k in kb:
+            # print k.xpath('string()').encode('utf-8')
+
+            # 通过 list 将课程信息添加到 PrettyTable 中
+            if i < 7:
+                list.append(
+                    k.text.split(';')[0].replace('<<', '').replace('>>', ''))
+                i += 1
+                if i == 7:
+                    # 上下显示一行空白，方便阅读
+                    x.add_row(list1)
+                    x.add_row(list)
+                    x.add_row(list1)
+                    list = []
+                    i = 0
+        print x
 
     # 教务通知
     def news(self):
-        newsurl = 'http://jw.qdu.edu.cn/homepage/infoArticleList.do;jsessionid=E06A6E2B5FA3F797FAB8FA5F6331AC92?columnId=358'
-        news = BeautifulSoup(requests.get(newsurl).content, "lxml")
-        for tz in news.find_all(href=re.compile("articleId")):
-            print tz.get_text(strip=True)
-            print 'http://jw.qdu.edu.cn/homepage/' + tz.get('href')
+        newsurl = requests.get(
+            'http://jw.qdu.edu.cn/homepage/infoArticleList.do;jsessionid=E06A6E2B5FA3F797FAB8FA5F6331AC92?columnId=358')
+        html = etree.HTML(newsurl.content.decode('utf-8', 'ignore'))
+        news = html.xpath('//*[@id="thirdcontent"]/div[2]/ul/li/div/a')
+        for new in news:
+            print new.xpath('string(.)').encode('utf-8').replace(' ', '').replace('\n', '')
+            print 'http://jw.qdu.edu.cn/homepage/' + new.attrib['href']
             print
 
+    # 个人信息
+    def user(self):
+        print '======= 个人信息 ========'
+        print
+        user = self.s.get('http://jw.qdu.edu.cn/academic/showPersonalInfo.do')
+        html = etree.HTML(user.content.decode('utf-8', 'ignore'))
+        for n in html.xpath('/html/body/center/table[1]/tr[1]/td[2]/text()'):
+            print '姓名: ' + n.encode('utf-8')
+        for n in html.xpath('/html/body/center/table[1]/tr[2]/td[1]/text()'):
+            print '院系: ' + n.encode('utf-8')
+        for n in html.xpath('/html/body/center/table[1]/tr[2]/td[2]/text()'):
+            print '专业: ' + n.encode('utf-8')
+        for n in html.xpath('/html/body/center/table[1]/tr[4]/td[1]/text()'):
+            print '年级: ' + n.encode('utf-8')
+        for n in html.xpath('/html/body/center/table[1]/tr[4]/td[2]/text()'):
+            print '班级: ' + n.encode('utf-8')
 
 jw = qdujw()
 print '======== 教务通知 ========'
@@ -175,7 +217,8 @@ while 1:
     print
     print '[1]查询成绩'
     print '[2]查询课表'
-    print '[3]退出'
+    print '[3]个人信息'
+    print '[4]退出'
     print
     choice = raw_input('请选择: ')
     print
@@ -184,4 +227,6 @@ while 1:
     elif choice == '2':
         jw.kebiao()
     elif choice == '3':
+        jw.user()
+    elif choice == '4':
         exit()
